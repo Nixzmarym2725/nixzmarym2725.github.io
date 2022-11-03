2dde8ea5b9ad3f3fcfe49c593b7ec9642c2be7d2
Nixzmarym2725 <114611035+Nixzmarym2725@users.noreply.github.com>
2022-11-03 18:05:19 -0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Nixzmary M."; // update the part BETWEEN the "quotes"
+let userName = "nixzmarym2725"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
